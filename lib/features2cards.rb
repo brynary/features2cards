347f5f4e8608773b4108c37557a720223f521210
Bryan Helmkamp <bryan@brynary.com>
2009-11-09 14:34:55 -0500

    Prep for 0.4.0 release

--- lib/features2cards.rb
+++ lib/features2cards.rb
@@ -4,8 +4,8 @@ require "features2cards/cli"
 module Features2Cards#:nodoc:
   class VERSION #:nodoc:
     MAJOR = 0
-    MINOR = 3
-    TINY  = 1
+    MINOR = 2
+    TINY  = 0
     PATCH = nil # Set to nil for official release
 
     STRING = [MAJOR, MINOR, TINY, PATCH].compact.join('.')
